de1c6b36bb9ae6fedb5ab6490772a56aa6f005a9
Artur Chmura <achmura66@gmail.com>
2021-03-26 13:41:02 +0100

    first test completed

--- src/__tests__/pages/mainPage/rentPart/StationsList.test.tsx
+++ src/__tests__/pages/mainPage/rentPart/StationsList.test.tsx
@@ -1,36 +1,52 @@
-import 'regenerator-runtime/runtime'
-import { cleanup, render, act } from '@testing-library/react';
+import { act, cleanup, fireEvent } from '@testing-library/react';
 import React from 'react';
-import * as EEEEE from '../../../../api/stations/getStations';
+import 'regenerator-runtime/runtime';
+import { getBikesFromStation } from '../../../../api/bikes/getBikesFromStation';
+import { getStations } from '../../../../api/stations/getStations';
+import { Station } from '../../../../models/station';
+import { UnrentedBike } from '../../../../models/unrentedBike';
 import StationsList from '../../../../pages/mainPage/rentPart/StationsList';
-import { ThemeProvider } from '@material-ui/core';
-import { SnackbarProvider } from 'notistack';
-import mainTheme from '../../../../layout/mainTheme';
+import { render } from '../../../test-utils';
 
 afterEach(cleanup);
 
 jest.mock('../../../../api/stations/getStations');
+jest.mock('../../../../api/bikes/getBikesFromStation');
 
 
-const mockMyFunction = EEEEE.getStations as jest.MockedFunction<typeof EEEEE.getStations>;
+const mockedGetStations = getStations as jest.MockedFunction<typeof getStations>;
+const mockedGetBikesFromStation = getBikesFromStation as jest.MockedFunction<typeof getBikesFromStation>;
 
 it("Station card unfolds on click", async () => {
 
-    const stations = [{ id: "1", name: "Wadowice" }, { id: "2", name: "Na rynku w Barlinku" }, { id: "312", name: "Dom Rycha Peji" }, { id: "44124", name: "W Szczecinie na młynie" }];
+    const stations: Station[] = [{ id: "1", name: "Wadowice" }, { id: "2", name: "Na rynku w Barlinku" }, { id: "312", name: "Dom Rycha Peji" }, { id: "44124", name: "W Szczecinie na młynie" }];
     const resp = { isError: false, responseCode: 200, data: stations }
+    mockedGetStations.mockResolvedValue(resp);
 
-    mockMyFunction.mockResolvedValue(resp);
-    const wrapper = ({ children }: any) => (
-        <ThemeProvider theme={mainTheme}>
-            <SnackbarProvider maxSnack={3} >
-                {children}
-            </SnackbarProvider  >
-        </ThemeProvider >
-    )
+    const bikes: UnrentedBike[] = [{ id: "2137" }, { id: "2138" }];
+    const resp2 = { isError: false, responseCode: 200, data: bikes }
+    mockedGetBikesFromStation.mockResolvedValue(resp2);
+
+    let elo: any = {};
     await act(async () => {
-        const { queryByLabelText } = render(
-            <StationsList />, { wrapper }
+        elo = render(
+            <StationsList />
         );
+
     });
+    expect(elo.getByText('Wadowice')).toBeDefined();
+    expect(elo.getByText('Na rynku w Barlinku')).toBeDefined();
+    expect(elo.getByText('Dom Rycha Peji')).toBeDefined();
+    expect(elo.getByText('W Szczecinie na młynie')).toBeDefined();
+
+    expect(elo.queryByText('2137')).toBeNull();
+    expect(elo.queryByText('2138')).toBeNull();
+
+    await act(async () => {
+        fireEvent.click(elo.getByText('Wadowice'));
+    })
+
+    expect(elo.getByText('2137')).toBeDefined();
+    expect(elo.getByText('2138')).toBeDefined();
 
 })
