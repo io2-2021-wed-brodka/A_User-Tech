3ad072a847bd40085018d6a7dcfc49f2cce703ee
Maciej Chlebny <chlebny59@gmail.com>
2021-05-13 12:35:59 +0200

    malfunctions view

---
+++ src/pages/tech/MalfunctionsTable.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { makeStyles, Theme, createStyles, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
+import { useSnackbar } from 'notistack';
+import { Malfunction } from '../../models/malfunctions';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),      
+      display: "flex",
+      width: "100%",
+      justify: "center", 
+      alignItems: "center",      
+    },
+    table: {
+      minWidth: 700,
+    },
+    blockButton: {
+      color: "#ee6002"
+    },
+    unblockButton: {
+        color: "#09af00"
+    },
+    addButton: {
+      margin: theme.spacing(2),
+    },
+  })
+);
+
+export interface MalfunctionsTableProps {
+  setMalfunctions: (value: React.SetStateAction<Malfunction[]>) => void;  
+  malfunctions: Malfunction[];
+}
+
+const MalfunctionsTable = (props: MalfunctionsTableProps) => {
+    const classes = useStyles()
+    const { enqueueSnackbar } = useSnackbar();
+   
+  return (
+    <TableContainer component={Paper}>
+    <Table className={classes.table} aria-label="simple table">
+      <TableHead>
+        <TableRow>
+          <TableCell>Bike</TableCell>
+          <TableCell align="right">User Reported Id</TableCell>
+          <TableCell align="right">Bike Id</TableCell>
+          <TableCell align="right">Description</TableCell>
+          <TableCell align="center" colSpan={3}>Action</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {props.malfunctions.map((malf) => (
+          <TableRow key={malf.id}>
+            <TableCell component="th" scope="row">
+              Malfunction {malf.id}
+            </TableCell>
+            <TableCell align="right">{malf.bikeId}</TableCell>
+            <TableCell align="right">
+              {malf.reportingUserId}
+            </TableCell>
+            <TableCell align="right">{malf.description ?? "-"}</TableCell>
+            <TableCell align="right">
+                <Button className={classes.blockButton} disabled>
+                    Approve
+                </Button>
+            </TableCell>
+            <TableCell align="center">
+              <Button color="secondary" disabled>
+                Deny
+              </Button>
+            </TableCell>
+            <TableCell align="center">
+              <Button color="primary" disabled>
+                Fixed
+              </Button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
+  );
+}
+
+export default MalfunctionsTable;
